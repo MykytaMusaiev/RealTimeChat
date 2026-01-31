a2eb64f9e119b526a49adf75a02a61b99d50e55c
Mykyta Musaiev <n.musaiev@gmail.com>
2026-01-31 19:47:32 +0200

    minor fix

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,5 +1,5 @@
 import { treaty } from "@elysiajs/eden";
-import type { app } from "../app/api/[[...slugs]]/route";
+import type { App } from "../app/api/[[...slugs]]/route";
 
 // TODO change after deploy
-export const client = treaty<app>("localhost:3000").api;
+export const client = treaty<App>("localhost:3000").api;
